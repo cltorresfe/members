b6bcf144a74577fd0f815b0c220f2fffe35fc466
Claudio Torres <cltorresfe@gmail.com>
2016-04-01 12:33:49 -0300

    Agregar estilos en la lista de miembros y church, agrega email cuando se crea un miembro

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-s1 = Status.create(name: 'Baja')
+s1 = Status.create(name: 'Activo')
+ch1 = Church.create(name: 'Asamblea de Dios Autónoma', adress: 'J.M. de Rosas 1154',
+                    email: 'raul.torres.calistro@gmail.com', phone: 11151515 )
 
-ch3 = Church.last
- 
+ch2 = Church.create(name: 'Presbiteriana', adress: 'Luis Araneda 2323',
+                    email: 'presbiteriana@gmail.com', phone: 11991515 ) 
 10.times do |u|
   Member.create(name: Faker::Name.name, email: Faker::Internet.email, adress: Faker::Address.street_address,
-  phone: Faker::Number.number(8), status: s1, church: ch3 )
+  phone: Faker::Number.number(8), status: s1, church: ch1 )
+end
+15.times do |u|
+  Member.create(name: Faker::Name.name, email: Faker::Internet.email, adress: Faker::Address.street_address,
+  phone: Faker::Number.number(8), status: s1, church: ch2 )
 end
