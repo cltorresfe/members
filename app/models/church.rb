8e8db4bdb64756aaed86b744cc4b97bcd29b966c
Alter Lagos <alter.strife@gmail.com>
2016-04-19 18:19:59 -0300

    Asociando usuarios con iglesias

--- app/models/church.rb
+++ app/models/church.rb
@@ -1,6 +1,8 @@
 class Church < ActiveRecord::Base
-	has_many :members, inverse_of: :order
-	validates :name, :adress, :email, :phone,  presence: true
-	validates :name, :email, uniqueness: true
+  has_many :members, inverse_of: :order
+  has_many :users
+
+  validates :name, :adress, :email, :phone,  presence: true
+  validates :name, :email, uniqueness: true
 
 end
