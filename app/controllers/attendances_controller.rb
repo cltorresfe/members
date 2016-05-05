0699cf8f93d0d5a6008e4769248c069139d34b09
Alter Lagos <alter.strife@gmail.com>
2016-05-05 17:49:32 -0300

    Formulario de asistencia guarda informacion

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,17 +1,27 @@
 class AttendancesController < ApplicationController
 
   def index
-    # @attendance = Attendance.new
     @current_ministry = params[:ministry_id] ? Ministry.find(params[:ministry_id]) : Ministry.first
     @attendance_date = params[:attendance_date] || Date.today.strftime('%d/%m/%Y')
-    @attendances = Attendance.where(ministry: @current_ministry, attendance_date: @attendance_date)
+    @attendances = Attendance.where(ministry: @current_ministry, attendance_date: Date.parse(@attendance_date).beginning_of_day)
     @charges = @current_ministry.charges.non_administrative
-
     @ministries = Ministry.all
   end
 
   def create
-    redirect_to action: :index
+    attendances = params[:attendance]
+    if attendances && attendances.any?
+      attendances.each do |attendance_params|
+        attendance = Attendance.find_or_initialize_by(
+          member_id: attendance_params[:member_id],
+          ministry_id: params[:ministry_id],
+          attendance_date: Date.parse(params[:attendance_date]).beginning_of_day
+        )
+        attendance.present = attendance_params[:present].present?
+        attendance.save
+      end
+    end
+    redirect_to action: :index, attendance_date: params[:attendance_date], ministry_id: params[:ministry_id]
   end
 
   private
