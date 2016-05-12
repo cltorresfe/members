a3f51b97f5f4dcddbfc50d45a1033c9d0cb42f09
Alter Lagos <alter.strife@gmail.com>
2016-05-12 11:23:02 -0300

    sacando tests de minitest

--- test/mailers/previews/welcome_member_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/welcome_member
-class WelcomeMemberPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/welcome_member/notify
-  def notify
-    WelcomeMember.notify Member.first
-  end
-
-end
