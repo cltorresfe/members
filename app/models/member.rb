80e9371c01ccf6ad94f0734ebdc9cf45744398e7
Alter Lagos <alter.strife@gmail.com>
2016-05-12 11:23:39 -0300

    busqueda no debe entregar nil si no vienen criterios de busqueda

--- app/models/member.rb
+++ app/models/member.rb
@@ -14,22 +14,24 @@
 #
 
 class Member < ApplicationRecord
-	belongs_to :church
-	belongs_to :status
-    has_many :charge_members
-	has_many :charges, through: :charge_members
+  belongs_to :church
+  belongs_to :status
+  has_many :charge_members
+  has_many :charges, through: :charge_members
 
-	validates :name, :adress, :email, :phone, :church, presence: true
-	validates :email, uniqueness: true, email: true
-	validates :phone, numericality: { less_than_or_equal_to: 2147483647 }
-	validates :name, length: { maximum: 35,
-    	too_long: "%{count} characters is the maximum allowed" }
-    validates :adress, length: { maximum: 50,
-    	too_long: "%{count} characters is the maximum allowed" }
+  validates :name, :adress, :email, :phone, :church, presence: true
+  validates :email, uniqueness: true, email: true
+  validates :phone, numericality: { less_than_or_equal_to: 2147483647 }
+  validates :name, length: { maximum: 35,
+            too_long: "%{count} characters is the maximum allowed" }
+  validates :adress, length: { maximum: 50,
+            too_long: "%{count} characters is the maximum allowed" }
 
-    def self.search(search)
-    	if search
-    		where('name LIKE ?', "%#{search}%")
-    	end
+  def self.search(search)
+    if search
+      where('name LIKE ?', "%#{search}%")
+    else
+      none
     end
+  end
 end
