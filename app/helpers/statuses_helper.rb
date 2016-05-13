5b044f3065280c09d3c6ce89e9f0ef2919cfa3ce
Alter Lagos <alter.strife@gmail.com>
2016-05-13 11:32:59 -0300

    Borrando referencias a modelo status

--- app/helpers/statuses_helper.rb
+++
@@ -1,5 +0,0 @@
-module StatusesHelper
-		def statuses_for_select
-  		Status.all.collect { |c| [c.name, c.id] }
-	end
-end
---
+++ db/migrate/20160513141707_remove_statuses_table.rb
@@ -0,0 +1,5 @@
+class RemoveStatusesTable < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :statuses
+  end
+end
