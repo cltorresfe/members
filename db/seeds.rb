d5176b2d390d652afb94d2604839844909c72fd1
Alter Lagos <alter.strife@gmail.com>
2016-05-13 18:01:53 -0300

    Agregando estado random a usuarios seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-s1 = Status.find_or_create_by(name: 'Pasivo')
 
 ch3 = Church.find_or_create_by!(name: 'Presbiteriana', email: Faker::Internet.email, address: Faker::Address.street_address, phone: Faker::Number.number(8))
 
 10.times do |u|
   Member.create!(name: Faker::Name.name, email: Faker::Internet.email, address: Faker::Address.street_address,
-  phone: Faker::Number.number(8), status: s1, church: ch3 )
+  phone: Faker::Number.number(8), status: Member.statuses.keys.sample, church: ch3 )
 end
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
