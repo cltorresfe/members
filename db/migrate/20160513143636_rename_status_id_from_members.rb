cb02a8f95b1fcf4fa360a1eefccd40cfd72c0b40
Alter Lagos <alter.strife@gmail.com>
2016-05-17 15:00:43 -0600

    Renombrar columna solo cuando este creada la tabla de statuses

--- db/migrate/20160513143636_rename_status_id_from_members.rb
+++ db/migrate/20160513143636_rename_status_id_from_members.rb
@@ -1,5 +1,9 @@
 class RenameStatusIdFromMembers < ActiveRecord::Migration[5.0]
   def change
-    rename_column :members, :status_id, :status
+    if ActiveRecord::Base.connection.table_exists? :statuses
+      rename_column :members, :status_id, :status
+    else
+      add_column :members, :status, :integer
+    end
   end
 end
