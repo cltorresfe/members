2216c57c8d23c4b950053ba9b651ba4608b31544
Alter Lagos <alter.strife@gmail.com>
2016-05-17 14:55:12 -0600

    Remover tabla solo si existe

--- db/migrate/20160513141707_remove_statuses_table.rb
+++ db/migrate/20160513141707_remove_statuses_table.rb
@@ -1,5 +1,9 @@
 class RemoveStatusesTable < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :statuses
+  def up
+    # Remover tabla obsoleta solo en caso que ya exista
+    drop_table :statuses if ActiveRecord::Base.connection.table_exists? :statuses
+  end
+
+  def down
   end
 end
