ce94ca56e0f310200da4f0fdfd0cfbdf6476b978
Claudio Torres <cltorresfe@gmail.com>
2016-05-18 11:20:41 -0400

    se elimina ruta '/locale=es' en la aplicacion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,22 +4,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   protect_from_forgery with: :exception
-  before_filter :set_locale, :authenticate_user!
+  before_filter :authenticate_user!
 
   def authenticate_active_admin!
     unless current_user && current_user.admin?
       redirect_to root_path
     end
   end
-
-  private
-
-    def set_locale
-      I18n.locale = params[:locale] || I18n.default_locale
-    end
-
-    def default_url_options(options = {})
-      {locale: I18n.locale}
-    end
 
 end
