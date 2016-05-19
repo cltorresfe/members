35938764dd6919e9bd4a1700d8b11b20234c5258
Claudio Torres <cltorresfe@gmail.com>
2016-05-19 17:59:40 -0400

    fixing rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,11 +35,10 @@ RSpec.configure do |config|
 
   include Warden::Test::Helpers
   Warden.test_mode!
-  RSpec.configure do |config|
   config.include Warden::Test::Helpers
   config.before :suite do
     Warden.test_mode!
-  end
+
 end
 
   config.before :each do
