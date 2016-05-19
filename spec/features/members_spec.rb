4251e05ba12f4ae3579302ea521b5ac810be64fe
Claudio Torres <cltorresfe@gmail.com>
2016-05-19 17:00:37 -0400

    fixing test

--- spec/features/members_spec.rb
+++ spec/features/members_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 describe "Member index", :type => :feature do
   let!(:member) { create(:member, name: 'Homero')}
+  let!(:user) {create(:user)}
 
-  it 'loads all the members' do
-    visit root_path
-    expect(page).to have_content 'Homero'
+  context "cuando el usuario está logeado" do
+    before do
+      login_as user
+    end
+    it 'loads all the members' do
+      visit root_path
+      expect(page).to have_content 'Homero'
+    end
   end
+
+  context " cuando el usuario no está logeado" do |variable|
+    it 'loads message login' do
+      visit root_path
+      expect(page).to have_content 'iniciar sesión'
+    end
+  end
+
 end
