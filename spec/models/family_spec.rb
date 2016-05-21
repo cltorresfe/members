6135c53ae28a04d3e99a18be19279a66c25df498
Alter Lagos <alter.strife@gmail.com>
2016-05-20 21:44:30 -0400

    family Model spec

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Family, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { is_expected.to belong_to(:church) }
+
+  it { is_expected.to validate_presence_of(:name) }
+
 end
