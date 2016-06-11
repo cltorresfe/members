b9a2d7e98129e44ff1b4f031b636421a1dfc3a5e
Claudio Torres <cltorresfe@gmail.com>
2016-06-10 20:11:05 -0400

    changing the way to solve test

--- spec/models/responsibility.rb
+++ spec/models/responsibility.rb
@@ -3,33 +3,23 @@ require 'rails_helper'
 RSpec.describe Responsibility, :type => :model do
 
   describe '#by_church' do
+    let!(:church) { create(:church)}
+    let!(:member) { create(:member, church: church)}
+    let!(:responsibility) { create(:responsibility)}
+
+    subject {Responsibility.by_church(church)}
 
     context 'returns an array with the responsibilities of church' do
-      let!(:church) { create(:church)}
-      let!(:member) { create(:member, church: church)}
-      let!(:responsibility) { create(:responsibility)}
       let!(:responsibilities) {[responsibility]}
       let!(:ministry) { create(:ministry, church: church, responsibilities: responsibilities)}
       let!(:charge) { create(:charge, responsibility:responsibility, ministry: ministry )}
       let!(:charge_member) { create(:charge_member, charge:charge, member: member )}
 
-      it 'returns an array' do
-        expect(Responsibility.by_church(church)).not_to be_empty 
-      end
+      it {is_expected.not_to be_empty}
     end
 
-    context 'returns an array empty without loaded responsibilities to church associated' do 
-      let!(:church) { create(:church)}
-      let!(:member) { create(:member, church: church)}
-      let!(:responsibility) { create(:responsibility)}
-      it 'returns an array' do
-        expect(Responsibility.by_church(church)).to be_empty 
-      end
-      
+    context 'returns an array empty without loaded responsibilities to church associated' do
+      it {is_expected.to be_empty}
     end
-
   end
-
-
 end
-
