232f842607f923e5686d6ac9c03b4b192721afbe
Claudio Torres <cltorresfe@gmail.com>
2016-06-13 19:29:46 -0400

    changing test

--- spec/models/responsibility_spec.rb
+++ spec/models/responsibility_spec.rb
@@ -4,14 +4,11 @@ RSpec.describe Responsibility, :type => :model do
 
   describe '#by_church' do
     let!(:church) { create(:church)}
-    let!(:member) { create(:member, church: church)}
-    let!(:responsibility) { create(:responsibility)}
 
     subject {Responsibility.by_church(church)}
 
     context 'returns an array with the responsibilities of church' do
-      let!(:ministry) { create(:ministry, church: church, responsibilities: [responsibility])}
-      let!(:charge) { create(:charge, responsibility:responsibility, ministry: ministry, members: [member] )}
+      let!(:responsibility) { create(:responsibility, church: church)}
 
       it {is_expected.not_to be_empty}
     end
