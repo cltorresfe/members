078ef823e28c7573495bc98c0e8f1a3aaf9a10c0
Claudio Torres <cltorresfe@gmail.com>
2016-06-15 15:14:06 -0400

    adding test when phone contains a character

--- app/models/church.rb
+++ app/models/church.rb
@@ -20,6 +20,7 @@ class Church < ApplicationRecord
   validates :name, :address, presence: true
   validates :name, :email, uniqueness: true
   validates :phone, length: { maximum: 12, minimum: 7 }
+  
   before_validation :change_to_format_phone
 
   protected
