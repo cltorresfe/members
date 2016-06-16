63c848cf61ae3e91a7965705b12781ec48b7f593
Alter Lagos <alter.strife@gmail.com>
2016-06-16 11:24:59 -0400

    Commenting not used mailers

--- app/mailers/welcome_member.rb
+++ app/mailers/welcome_member.rb
@@ -1,13 +1,13 @@
-class WelcomeMember < ApplicationMailer
+# class WelcomeMember < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.welcome_member.notify.subject
-  #
-  def notify(member)
-    @greeting = "Hi"
-    @member =member
-    mail to: @member.email, subject: 'Bienvenido!!'
-  end
-end
+#   # Subject can be set in your I18n file at config/locales/en.yml
+#   # with the following lookup:
+#   #
+#   #   en.welcome_member.notify.subject
+#   #
+#   def notify(member)
+#     @greeting = "Hi"
+#     @member =member
+#     mail to: @member.email, subject: 'Bienvenido!!'
+#   end
+# end
