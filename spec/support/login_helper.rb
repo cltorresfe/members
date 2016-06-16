ad00b57217268c2203e81d9bba06b07a341895fc
Alter Lagos <alter.strife@gmail.com>
2016-06-16 18:22:26 -0400

    Helper to login in feature specs

---
+++ spec/support/login_helper.rb
@@ -0,0 +1,12 @@
+module LoginHelper
+  def log_in user
+    visit new_user_session_path
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button "Iniciar sesión"
+  end
+
+  def log_out
+    first(:link, 'Cerrar').click
+  end
+end
