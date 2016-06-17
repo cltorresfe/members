aeecb7bdcf1e11b7b8cc334f79d6e033f89ab11a
Alter Lagos <alter.strife@gmail.com>
2016-06-16 23:42:10 -0400

    Removing some deprecation warnings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   protect_from_forgery with: :exception
-  before_filter :authenticate_user!, :clear_flash, :has_church_assigned
+  before_action :authenticate_user!, :clear_flash, :has_church_assigned
 
   def authenticate_active_admin!
     unless current_user && current_user.admin?
