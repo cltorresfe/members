890593bb2716bb4c911c784dbda8030cd39e0353
Alter Lagos <alter.strife@gmail.com>
2016-06-18 21:29:19 -0400

    Member display refactoring

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,11 +5,12 @@ class MainController < ApplicationController
     @responsibilities = Responsibility.by_church(current_user.church)
     @families = current_user.church.families
     @attendances = Attendance.attendances_last_week(current_user.church)
-    gon.attendances_week = @attendances if @attendances.present?
-    gon.members_gender = current_user.church.members.by_gender
-    gon.members_range = current_user.church.members.by_range
     @members_last = @members.last(8)
     @members_birth_date = @members.birth_date_by_month
     @families_married_date = @families.married_notification_by_month
+
+    gon.attendances_week = @attendances if @attendances.present?
+    gon.members_gender = current_user.church.members.by_gender
+    gon.members_range = current_user.church.members.by_range
   end
 end
