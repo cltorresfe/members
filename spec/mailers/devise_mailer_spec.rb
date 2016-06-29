ed434f482246aeed01be19fff5ad7f53a8686b4b
Alter Lagos <alter.strife@gmail.com>
2016-06-28 23:56:54 -0400

    No need for line

--- spec/mailers/devise_mailer_spec.rb
+++ spec/mailers/devise_mailer_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Devise::Mailer, :type => :mailer do
   let(:user){ create(:user)}
 
   it "enqueues the devise mails" do
-    ActiveJob::Base.queue_adapter = :test
     expect {
       user.send_reset_password_instructions
     }.to have_enqueued_job.on_queue('mailers')
