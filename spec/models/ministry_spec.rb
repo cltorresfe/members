3570774119ad262dbbb349f03c96efe13582d73c
Claudio Torres <cltorresfe@gmail.com>
2016-06-29 12:23:29 -0400

    adding test for percent_attendances_by_member

--- spec/models/ministry_spec.rb
+++ spec/models/ministry_spec.rb
@@ -54,4 +54,27 @@ RSpec.describe Ministry, :type => :model do
       it {is_expected.to be_empty}
     end
   end
+
+  describe '.percent_attendances_by_member' do
+
+    let!(:ministry) { create(:ministry, :with_responsibilities, church: church )}
+    let!(:member) { create(:member, church: church)}
+    subject {ministry.percent_attendances_by_member(member)}
+
+    context 'returns a percent with the attendance of member' do
+      let!(:attendance) { create(:attendance, member: member, ministry: ministry)}
+
+      it {is_expected.to eq(100)}
+    end
+
+    context 'returns a percent valid without the attendance of member' do
+      let!(:attendance) { create(:attendance, member: member, ministry: ministry, present: false)}
+
+      it {is_expected.to eq(0)}
+    end
+
+    context 'returns nil without loaded attendances to member associated' do
+      it {is_expected.to eq(nil)}
+    end
+  end
 end
