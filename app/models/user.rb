f9521aee174990f213f13d61605e8f43294cd896
Alter Lagos <alter.strife@gmail.com>
2016-06-28 21:56:04 -0400

    sending devise emails async

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,4 +48,9 @@ class User < ActiveRecord::Base
     church.members.search(search)
   end
 
+  # To send devise emails asynchronously
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
+
 end
