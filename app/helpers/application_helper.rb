4c99d8927d496c8aadbfdd4e0ba4e9211c2da02d
Claudio Torres <cltorresfe@gmail.com>
2016-07-06 17:32:14 -0400

    adding avatar view on member list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,20 @@ module ApplicationHelper
     %w(CL)
   end
 
+  def avatar_for(member, options = { size: 80})
+    size = options[:size]
+    if member.avatar?
+      image_tag member.avatar.url(:thumb), class:'img-circle img-container', width: size
+    else
+      imagen_file = case member.gender
+              when true then "default_avatar_female.png"
+              when false then "default_avatar_male.png"
+              when nil then "default_avatar.png"
+              end
+      image_tag imagen_file, class:'img-circle img-container', width: size
+
+    end
+
+  end
+
 end
