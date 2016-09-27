2e53875e6779210121bde0cbd2ddd8b30c8536ae
Claudio Torres <cltorresfe@gmail.com>
2016-09-27 17:24:21 -0300

    adding gon gem for graphs chart tithes

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -7,10 +7,12 @@ class MembersController < ApplicationController
   def show
     @ministries = @member.ministries
     @tithe = Tithe.new  
+    @tithes = @member.tithes_by_date(1.year.ago, Time.now).sorted
     @responsibilities = @member.responsibilities
     @attendances = @member.attendances.sorted
                           .paginate(page: params[:page], per_page: 12)
     @families = current_church.families
+    gon.tithes_year = @tithes if @tithes.present?
     if @member.family
       @family = @member.family
       @members_family = @family.members
