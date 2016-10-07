630d3e75c8885cfef408f7b354d253e64406666b
Claudio Torres <cltorresfe@gmail.com>
2016-10-07 15:40:58 -0300

    adding components ckeditor to precompile on production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( components.js )
+config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+config.assets.precompile += Ckeditor.assets
+config.assets.precompile += %w( ckeditor/* components.js react-server.js)
